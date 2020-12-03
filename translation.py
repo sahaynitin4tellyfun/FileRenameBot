d995614012f6c1ac194c005b12c7d7eea71a330e
Ayush Sharma <aayush240204@gmail.com>
2020-12-03 09:16:45 +0530

    Initial Commit

---
+++ translation.py
@@ -0,0 +1,37 @@
+class Translation(object):
+    START_TEXT = """ <b> Hi {} ,Hi I Am A Rename/Convert 2 Video Bot With Custom Thumbnail Support😍️</b> \n
+
+<b>Bot Maintained By: @AYushBots 😎️</b> \n
+<b>Do /help for more...</b> \n
+"""
+
+    RENAME_403_ERR = "What Are You Doing? Bruuh!!"
+    BANNED_USER_TEXT = "Sorry!! But Mai Owner As Banned You From Using This Service. That Means You Can't Run Meh Now! \n \n Contact : @CyberBoyAyush For more Details.. " 
+    ABS_TEXT = "What Are You Trying To Do,Mate?"
+    UPGRADE_TEXT = "CONTACT @CyberBoyAyush "
+    DOWNLOAD_START = "<b>Downloading To My server !! pls Wait</b>"
+    UPLOAD_START = "<b>Downloading Completed Now I'm Uploading Into TG</b>"
+    RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry WTF Do You Think! I'll Upload It?"
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "**Thank you for Using Me Join Updates Channel> @AyushBots  ❤️**"
+    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds.\nUploaded in {} seconds"
+    NOT_AUTH_USER_TEXT = "CONTACT @CyberBoyAyush "
+    NOT_AUTH_USER_TEXT_FILE_SIZE = "IF You Got This Message You Should Contact @CyberBoyAyush "
+    SAVED_CUSTOM_THUMB_NAIL = "<b>thumbnail Saved ✅ This Is Permanent Until</b> /deletethumbnail ❤"
+    DEL_ETED_CUSTOM_THUMB_NAIL = "Thumbnail Cleared Succesfully😐️"
+    FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "Media cleared succesfully."
+    SAVED_RECVD_DOC_FILE = "<b>File Downloaded Successfully 😎</b>"
+    REPLY_TO_DOC_FOR_RENAME_FILE = "<b>Please Reply To An File With /rename fil name extension To rename a file</b>"
+    REPLY_TO_DOC_FOR_C2V = "<b> Please Reply To An File With /c2v To Convert It Into Streamable video File</b>"
+    REPLY_TO_DOC_FOR_C2A = "<b> Please Reply To An File/video With /c2a To Convert It Into Audio File</b>"
+    CUSTOM_CAPTION_UL_FILE = " "
+    NO_CUSTOM_THUMB_NAIL_FOUND = "No Thumbnail found,SED🤒"
+    USER_ADDED_TO_DB = "User <a href='tg://user?id={}'>{}</a> added to {} till {}."
+    IFLONG_FILE_NAME = """You Gotta Be Kidding Me...Decrease The Number Of Letters😆😉"""
+    ABOUT_ME = """<b>Meh : \n An Telegram File Renamer Bot Which Can Rename A Telegram Files. \n You Can Set Permanent Thumbnail For The File So You Don't Have To Send Custom Thumbs All The Time.\n \n i can also convert files into videos \n Join @Mai_BoTs If You Found Meh Useful</b>"""
+    HELP_USER = """Ohh You Want Help?? 😅
+    
+1.🔹 <b>Send me any Telegram File.</b> \n
+2.🔹 <b>Reply to that message with /rename New file name.extnsion</b> \n
+3.🔹 <b> Send A Photo to make it as permanent Thumbnail</b> \n
+4.🔹 <b> I Can Also Convert Files Into Video through /c2v Command </b>   \n \n <b>Made With ❤️ By: @AyushBots/b>"""
+
